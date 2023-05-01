293eb3d15dcb6c5a5433e34cce71af3703c3251f
Bartotti <lalipop123@outlook.com>
2023-05-01 00:47:27 -0400

    css layout

--- pages/mainScreen.js
+++ pages/mainScreen.js
@@ -1,49 +1,47 @@
 import React, { useState } from "react";
 import CartItem from "./cartItem";
-import products from "./products";
+export const products = [
+  {
+    name: "plate 1",
+    price: 1.99,
+    image: "plate1.jpeg",
+    description: "zxc123",
+  },
+  {
+    name: "plate 2",
+    price: 2.99,
+    image: "plate2.jpeg",
+    description: "acc1723",
+  },
+  {
+    name: "plate 3",
+    price: 3.99,
+    image: "plate3.jpeg",
+    description: "zbzc53",
+  },
+  {
+    name: "plate 4",
+    price: 4.99,
+    image: "plate4.jpeg",
+    description: "1zad53",
+  },
+  {
+    name: "plate 5",
+    price: 5.99,
+    image: "plate5.jpeg",
+    description: "5123",
+  },
+  {
+    name: "plate 6",
+    price: 6.99,
+    image: "plate6.jpeg",
+    description: "ad123c53",
+  },
+];
 
-// const products = [
-//   {
-//     name: "plate 1",
-//     price: 1.99,
-//     image: "plate1.jpeg",
-//     description: "zxc123",
-//   },
-//   {
-//     name: "plate 2",
-//     price: 2.99,
-//     image: "plate2.jpeg",
-//     description: "acc1723",
-//   },
-//   {
-//     name: "plate 3",
-//     price: 3.99,
-//     image: "plate3.jpeg",
-//     description: "zbzc53",
-//   },
-//   {
-//     name: "plate 4",
-//     price: 4.99,
-//     image: "plate4.jpeg",
-//     description: "1zad53",
-//   },
-//   {
-//     name: "plate 5",
-//     price: 5.99,
-//     image: "plate5.jpeg",
-//     description: "5123",
-//   },
-//   {
-//     name: "plate 6",
-//     price: 6.99,
-//     image: "plate6.jpeg",
-//     description: "ad123c53",
-//   },
-// ];
 export default function MainScreen() {
   const [cart, setCart] = useState([]);
   const [showCart, setShowCart] = useState(false);
-  const [isCartOpen, setIsCartOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [selectedQty, setSelectedQty] = useState(1);
 

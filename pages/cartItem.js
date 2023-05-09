fccad102ad229cd07648daf3b47ad9a10b88a9f3
Bartotti <lalipop123@outlook.com>
2023-05-08 23:17:02 -0400

    remove cart, update cart qty, and add cart same item +1 into cart

--- pages/cartItem.js
+++ pages/cartItem.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function CartItem({ cart }) {
+export default function CartItem({ cart, onRemove, onUpdate }) {
   const totalAmount = cart
     ? cart.reduce((acc, item) => acc + item.qty * item.price, 0)
     : 0;
+
+  function handleUpdateQty(itemId, newQty) {
+    onUpdate(itemId, newQty);
+  }
 
   return (
     <div>
@@ -13,8 +17,22 @@ export default function CartItem({ cart }) {
           {cart.map((item, index) => (
             <div key={index}>
               <p>{item.name}</p>
-              <p>Qty: {item.qty}</p>
+              <p>
+                Qty:
+                <input
+                  type="number"
+                  value={item.qty}
+                  onChange={event =>
+                    handleUpdateQty(item.id, parseInt(event.target.value))
+                  }
+                  min="1"
+                  max="998"
+                />
+              </p>
               <p>Price: ${item.price.toFixed(2)}</p>
+              <div>
+                <button onClick={() => onRemove(item.name)}>Remove</button>
+              </div>
               <p>--------------------------</p>
             </div>
           ))}
--- pages/mainScreen.js
+++ pages/mainScreen.js
@@ -4,36 +4,42 @@ import NavBar from "./navBar";
 
 export const products = [
   {
+    id: 1,
     name: "plate 1",
     price: 1.99,
     image: "plate1.jpeg",
     description: "zxc123",
   },
   {
+    id: 2,
     name: "plate 2",
     price: 2.99,
     image: "plate2.jpeg",
     description: "acc1723",
   },
   {
+    id: 3,
     name: "plate 3",
     price: 3.99,
     image: "plate3.jpeg",
     description: "zbzc53",
   },
   {
+    id: 4,
     name: "plate 4",
     price: 4.99,
     image: "plate4.jpeg",
     description: "1zad53",
   },
   {
+    id: 5,
     name: "plate 5",
     price: 5.99,
     image: "plate5.jpeg",
     description: "5123",
   },
   {
+    id: 6,
     name: "plate 6",
     price: 6.99,
     image: "plate6.jpeg",
@@ -49,10 +55,21 @@ export default function MainScreen() {
 
   function addToCart() {
     if (selectedProduct) {
-      setCart(prevCart => [
-        ...prevCart,
-        { ...selectedProduct, qty: selectedQty },
-      ]);
+      const itemExists = cart.some(item => item.name === selectedProduct.name);
+      if (itemExists) {
+        setCart(prevCart =>
+          prevCart.map(item =>
+            item.name === selectedProduct.name
+              ? { ...item, qty: item.qty + selectedQty }
+              : item
+          )
+        );
+      } else {
+        setCart(prevCart => [
+          ...prevCart,
+          { ...selectedProduct, qty: selectedQty },
+        ]);
+      }
       localStorage.setItem(
         "cart",
         JSON.stringify([...cart, { ...selectedProduct, qty: selectedQty }])
@@ -62,19 +79,49 @@ export default function MainScreen() {
     }
   }
 
-  // function toggleCart() {
-  //   setShowCart(prevShowCart => !prevShowCart);
-  // }
+  function removeFromCart(itemId) {
+    setCart(prevCart => prevCart.filter(item => item.name !== itemId));
+    localStorage.setItem(
+      "cart",
+      JSON.stringify(cart.filter(item => item.name !== itemId))
+    );
+    console.log("imclick");
+  }
+
   const toggleCart = () => {
     setShowCart(!showCart);
   };
+
+  function updateCartItemQty(productId, newQty) {
+    setCart(prevCart =>
+      prevCart.map(item =>
+        item.id === productId ? { ...item, qty: newQty } : item
+      )
+    );
+    localStorage.setItem(
+      "cart",
+      JSON.stringify(
+        cart.map(item =>
+          item.id === productId ? { ...item, qty: newQty } : item
+        )
+      )
+    );
+  }
   return (
     <div>
       <NavBar />
-      <div className="cart-button" onClick={toggleCart}>
-        Open/Close Cart ({cart.length})
-        {showCart && <CartItem cart={cart} onClose={toggleCart} />}
+      <div className="cart-button">
+        <button onClick={toggleCart}>Open/Close Cart</button>({cart.length})
+        {showCart && (
+          <CartItem
+            cart={cart}
+            onRemove={removeFromCart}
+            onUpdate={updateCartItemQty}
+            onClose={toggleCart}
+          />
+        )}
       </div>
+
       <div className="product-main">
         {products.map((product, index) => (
           <div
